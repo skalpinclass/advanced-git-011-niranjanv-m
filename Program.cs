b296c11553939c92617ef82322d286e79bd4aedd
Niranjan <niranjanv@magenic.com>
2020-04-23 11:07:38 -0400

    Testing the Branch rulke that should prevent checkin to master branch

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace advanced_git_001
             Console.WriteLine("Hello World!");
             Console.WriteLine("Added a new line without creating a new branch. Should go into the master branch");
             Console.WriteLine("Added a new line representing a feature to be added to branch_for_featire1 branch.");
+            Console.WriteLine("Added this line to see if the Branch rule works and it prevents me from checking ionto master branch.");
         }
     }
 }
